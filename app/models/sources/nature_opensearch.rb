3203baa6832fbdf03dd71a7ab3c088c195c746fa
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-04-24 17:04:06 +0100

    Start Source def for api.nature.com

---
+++ app/models/sources/nature_opensearch.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+
+# $HeadURL$
+# $Id$
+#
+# Copyright (c) 2009-2012 by Public Library of Science, a non-profit corporation
+# http://www.plos.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Nature < Source
+  def get_data(article, options={})
+    # Check that article has DOI
+    return { events: [], event_count: nil } if article.doi.blank?
+
+    query_url = get_query_url(article)
+    results = get_json(query_url, options)
+
+    return nil if results.nil?
+
+    events = results.map do |result|
+      url = result['post']['url']
+      url = "http://#{url}" unless url.start_with?("http://")
+
+      { :event => result['post'], :event_url => url }
+    end
+
+    { :events => events,
+      :event_count => events.length,
+      :event_metrics => event_metrics(citations: events.length) }
+  end
+
+  def get_config_fields
+    [{:field_name => "url", :field_type => "text_area", :size => "90x2"}]
+  end
+
+  def url
+    config.url || "http://blogs.nature.com/posts.json?doi=%{doi}"
+  end
+
+  def staleness_year
+    config.staleness_year || 1.month
+  end
+
+  def rate_limiting
+    config.rate_limiting || 200
+  end
+end
