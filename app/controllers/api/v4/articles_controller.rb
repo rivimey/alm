d982d35e1a13c4951f9c4b8f64cdbb25f511a92b
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-05-23 16:20:31 +0100

    render :status separately

--- app/controllers/api/v4/articles_controller.rb
+++ app/controllers/api/v4/articles_controller.rb
@@ -4,16 +4,39 @@ class Api::V4::ArticlesController < Api::V4::BaseController
 
   before_filter :load_article, :only => [ :update, :destroy ]
 
+  def show
+    # Load one article given query params
+    source_ids = get_source_ids(params[:source])
+
+    id_hash = { :articles => Article.from_uri(params[:id]), :retrieval_statuses => { :source_id => source_ids }}
+    @article = ArticleDecorator.includes(:retrieval_statuses).where(id_hash).decorate(context: { info: params[:info], source: params[:source] })
+
+    # Return 404 HTTP status code and error message if article wasn't found, or no valid source specified
+    if @article.blank?
+      if params[:source].blank?
+        @error = "Article not found."
+      else
+        @error = "Source not found."
+      end
+      render :status => :not_found
+      render "error"
+    else
+      @success = "Article found."
+    end
+  end
+
   def create
     @article = Article.new(safe_params)
     authorize! :create, @article
 
     if @article.save
       @success = "Article created."
-      render "success", :status => :created
+      render :status => :created
+      render "success"
     else
       @error = @article.errors
-      render "error", :status => :bad_request
+      render :status => :created
+      render "error"
     end
   end
 
@@ -22,13 +45,16 @@ class Api::V4::ArticlesController < Api::V4::BaseController
 
     if @article.blank?
       @error = "No article found."
-      render "error", :status => :not_found
+      render :status => :not_found
+      render "error"
     elsif @article.update_attributes(safe_params)
       @success = "Article updated."
-      render "success", :status => :ok
+      render :status => :ok
+      render "success"
     else
       @error = @article.errors
-      render "error", :status => :bad_request
+      render :status => :bad_request
+      render "error"
     end
   end
 
@@ -37,13 +63,16 @@ class Api::V4::ArticlesController < Api::V4::BaseController
 
     if @article.blank?
       @error = "No article found."
-      render "error", :status => :not_found
+      render :status => :not_found
+      render "error"
     elsif @article.destroy
       @success = "Article deleted."
-      render "success", :status => :ok
+      render :status => :ok
+      render "success"
     else
       @error = "An error occured."
-      render "error", :status => :bad_request
+      render :status => :bad_request
+      render "error"
     end
   end
 end
