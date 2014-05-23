1c8c31ff8f19ea9aa30fc77a7f390bc1c4a6eba4
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-05-23 13:14:03 +0100

    Include normal id_rsa ssh key as possible source (as well as vagrant one)

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -15,7 +15,7 @@ set :assets_roles, [:web, :app]
 
 server '33.33.33.44', roles: %w{web app db}
 
-set :ssh_options, user: "vagrant", keys: %w(~/.vagrant.d/insecure_private_key), auth_methods: %w(publickey)
+set :ssh_options, user: "vagrant", keys: %w(~/.vagrant.d/insecure_private_key ~/.ssh/id_rsa), auth_methods: %w(publickey)
 
 # Set number of delayed_job workers
 set :delayed_job_args, "-n 3"
